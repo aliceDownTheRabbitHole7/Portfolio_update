1e404414f85d1cf69676e1449cc1368b2e4909bc
Aleksander White <zander1116@aol.com>
2023-11-03 12:28:06 -0700

    minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import About from './pages/about/About'
 import Projects from './pages/projects/Projects'
